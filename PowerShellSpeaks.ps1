09000ca68405363ded3aa3d19a25100d0e3d565c
fixpcs <fixpcs@aol.com>
2018-02-15 14:59:15 -0500

    first commit

---
+++ PowerShellSpeaks.ps1
@@ -0,0 +1,42 @@
+﻿add-type -assemblyname system.speech 
+$PowerShellSpeak = New-Object System.Speech.Synthesis.SpeechSynthesizer 
+#Intro-Chrous 
+$PowerShellSpeak.speak("You Must Have Thought I Was Through") 
+$PowerShellSpeak.speak("You Should Have Killed Me When You Had The Chance") 
+$PowerShellSpeak.speak("I Can't Describe Whats Gonna Happen To You") 
+$PowerShellSpeak.speak("To You") 
+#Chorus 
+$PowerShellSpeak.speak("When Heads Roll") 
+$PowerShellSpeak.speak("The More The Better") 
+$PowerShellSpeak.speak("Strength, Determination, Merciless Forever") 
+$PowerShellSpeak.speak("When Heads Roll") 
+$PowerShellSpeak.speak("The More The Better") 
+$PowerShellSpeak.speak("Strength, Determination, Merciless Forever") 
+#2nd Verse 
+$PowerShellSpeak.speak("As I Lay Bleeding In My Pool Of Blood") 
+$PowerShellSpeak.speak("I Listened While You Laughed") 
+$PowerShellSpeak.speak("Retribution In It's Purest Form") 
+$PowerShellSpeak.speak("You Should Have Killed Me When You Had The Chance") 
+$PowerShellSpeak.speak("The Chance") 
+#Chorus 
+$PowerShellSpeak.speak("When Heads Roll") 
+$PowerShellSpeak.speak("The More The Better") 
+$PowerShellSpeak.speak("Strength, Determination, Merciless Forever") 
+$PowerShellSpeak.speak("When Heads Roll") 
+$PowerShellSpeak.speak("The More The Better") 
+$PowerShellSpeak.speak("Strength, Determination, Merciless Forever") 
+#Outro 
+$PowerShellSpeak.speak("When Heads Roll") 
+$PowerShellSpeak.speak("The More The Better") 
+$PowerShellSpeak.speak("Strength, Determination, Merciless Forever")
+$PowerShellSpeak.speak("When Heads Roll") 
+$PowerShellSpeak.speak("The More The Better") 
+$PowerShellSpeak.speak("Strength, Determination, Merciless Forever") 
+$PowerShellSpeak.speak("When Heads Roll") 
+$PowerShellSpeak.speak("The More The Better") 
+$PowerShellSpeak.speak("Strength, Determination, Merciless Forever")
+$PowerShellSpeak.speak("When Heads Roll") 
+$PowerShellSpeak.speak("The More The Better") 
+$PowerShellSpeak.speak("Strength, Determination, Merciless Forever") 
+ 
+$PowerShellSpeak.Dispose()
